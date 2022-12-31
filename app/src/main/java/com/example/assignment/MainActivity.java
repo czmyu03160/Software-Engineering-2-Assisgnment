9af12c2aeddf9fa21426dd06b519f89595ebc580
JayYu <jayuhk@gmail.com>
2022-12-31 21:49:24 +0800

    second commit

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -12,24 +12,31 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
 
+    // Declare UI elements
+
     EditText password, Email;
     Button button1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize UI elements
+
         password = findViewById(R.id.password);
         Email = findViewById(R.id.Email);
-
-
         button1=(Button) findViewById(R.id.button);
+
+
+        // Set up click listener for login button
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                // Set up the condition when the email and password are not enter, it will show the error message
 
                 if (Email.getText().toString().isEmpty() || !Email.getText().toString().contains("@")) {
                     Email.setError("Enter your email!");
@@ -38,7 +45,10 @@ public class MainActivity extends AppCompatActivity {
                     password.setError("Enter your password!");
                     Toast.makeText(MainActivity.this, "Invalid username or password",Toast.LENGTH_LONG).show();;
                 }  else {
-                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+
+                    // If email and password are correct,  login is successful and open employee home
+
+                    Intent intent=new Intent(MainActivity.this,MainActivity2.class);
                 startActivity(intent); }
                 }
         });
--- app/src/main/java/com/example/assignment/MainActivity2.java
+++ app/src/main/java/com/example/assignment/MainActivity2.java
@@ -21,6 +21,8 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+
+    // Declare UI element for notification button
     private ImageButton notificationBtn;
 
 
@@ -29,36 +31,48 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-
-
-
+        // Initialize drawer layout
         final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
 
+
+        // Set click listener for image menu view
         findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // Open navigation drawer when image menu view is clicked
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
+        // Initialize navigation view
         NavigationView navigationView = findViewById(R.id.navigationView);
 
+
+        // Find nav host fragment and set up navigation controller
         NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
         NavigationUI.setupWithNavController(navigationView, navController);
 
 
+        // Initialize notification button
         notificationBtn = (ImageButton) findViewById(R.id.bt_Notification);
+
+        // Check if Android version is Oreo or higher
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O )
         {
+            // Create notification channel
             NotificationChannel nc = new NotificationChannel("01", "Channel", NotificationManager.IMPORTANCE_DEFAULT);
             NotificationManager nm = getSystemService(NotificationManager.class);
             nm.createNotificationChannel(nc);
         }
 
 
+        // Set up click listener for notification button
         notificationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // Create notification using NotificationCompat.Builder
                 NotificationCompat.Builder ncb = new NotificationCompat.Builder(getApplicationContext(),"01");
                 ncb.setSmallIcon(R.drawable.ic_baseline_message_24);
                 ncb.setContentTitle("Notification");
@@ -68,15 +82,5 @@ public class MainActivity2 extends AppCompatActivity {
                 nmc.notify(1, ncb.build());
             }
         });
-
-
-
-
-
-
-
-
-
-
     }
 }
